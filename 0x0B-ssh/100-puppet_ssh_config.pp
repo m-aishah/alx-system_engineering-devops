a8198f567b1ee67379a13066c51272952165daa9
Aishah <maishah2540@gmail.com>
2023-10-29 13:03:00 -0700

    Task 4.

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+# Define SSH client configuration
+file { '/etc/ssh/sshd_config':
+  ensure  => 'file',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  content => '
+  IdentityFile ~/.ssh/school
+  PasswordAuthentication no
+',
+}
+
+# Ensure correct permissions on the SSH configuration file
+file { '/etc/ssh':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0700',
+}
