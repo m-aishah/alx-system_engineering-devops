015aa06ae27baf01bcc32d332806d5f27c26f798
Aishah <maishah2540@gmail.com>
2023-11-25 02:50:28 -0800

    Using regexes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,33 @@
-# Define SSH client configuration
-file { '/etc/ssh/ssh_config':
-  ensure  => 'file',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  content => '
-  IdentityFile ~/.ssh/school
-  PasswordAuthentication no
-',
-}
-
-# Ensure correct permissions on the SSH configuration file
+# Ensure correct permissions on the SSH directory
 file { '/etc/ssh':
   ensure => 'directory',
   owner  => 'ubuntu',
   group  => 'ubuntu',
   mode   => '0700',
+}
+
+# Define SSH client configuration
+file { '/etc/ssh/ssh_config':
+  ensure  => 'file',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+}
+
+# Include the stdlib module
+include stdlib
+
+# Manage IdentityFile line in SSH client configuration
+file_line { 'ssh_identity_file':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^#*IdentityFile\s.*$',
+  ensure  => present,
+}
+
+# Manage PasswordAuthentication line in SSH client configuration
+file_line { 'ssh_password_authentication':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   => '^#*PasswordAuthentication\s.*$',
+  ensure  => present,
 }
