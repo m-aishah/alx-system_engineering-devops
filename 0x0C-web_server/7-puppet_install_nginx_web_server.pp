662a88985c6a9904ed4112c0d0b8db4f16b6a43c
Aishah <maishah2540@gmail.com>
2023-11-25 04:33:51 -0800

    Task 5.

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,42 @@
-# Install Nginx package
-package { 'nginx':
-  ensure => 'installed',
-}
+# Define a class to install and configure Nginx
+class nginx {
+  package { 'nginx':
+    ensure => installed,
+  }
 
-# Create a new Nginx configuration file
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => '
-    server {
-        listen 80;
-        listen [::]:80 default_server;
-        root   /etc/nginx/html;
-        index  index.html index.htm;
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-        location /redirect_me {
-            return 301 http://cuberule.com/;
-        }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
     }
-  ',
-  require => Package['nginx'], # Ensure the Nginx package is installed before creating the configuration file
+
+    location / {
+        alias /var/www/html;
+    }
+}
+",
+    require => Package['nginx'],
+  }
+
+  file { '/var/www/html/index.html':
+    ensure  => file,
+    content => "Hello World!\n",
+    require => Package['nginx'],
+  }
+
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => [File['/etc/nginx/sites-available/default'], File['/var/www/html/index.html']],
+  }
 }
 
-# Create the "Hello World" index.html
-file { '/etc/nginx/html/index.html':
-  ensure  => 'file',
-  content => 'Hello World!',
-  require => Package['nginx'], # Ensure the Nginx package is installed before creating the index.html file
-}
-
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => [
-    File['/etc/nginx/sites-available/default'], # Ensure the configuration file is in place before starting the service
-    File['/etc/nginx/html/index.html'],         # Ensure the index.html file is in place before starting the service
-  ],
-}
+# Include the nginx class to configure Nginx
+include nginx
