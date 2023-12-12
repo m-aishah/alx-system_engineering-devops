8508a950d4051aa9b6ac5780aada6aac8c466e7e
Aishah <maishah2540@gmail.com>
2023-12-12 07:16:29 -0800

    Task 1.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Queries the Reddit API and prints the titles of
+the first 10 hot posts listed for a given subreddit.
+'''
+import requests
+
+
+def top_ten(subreddit):
+    ''' Prints the title of the top 10 hot posts for a given subreddit.
+
+    subreddit (str): The name of the subreddit in question.
+    '''
+
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    params = {'limit': 10}
+    response = requests.get(url, params=params, allow_redirects=False)
+
+    if response.status_code == 404:
+        print(None)
+        return
+
+    posts = response.json().get('data')
+    [print(post.get('data').get('title')) for post in posts.get('children')]
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -12,5 +12,11 @@ This project is a programming assignment focused on utilizing Python for working
 
 - Description: Write a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.
 
-- File: [00-subs.py](./0-subs.py)
+- File: [0-subs.py](./0-subs.py)
 
+
+<b>1.Top Ten</b>
+
+- Description: Write a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+
+- File: [1-top_ten.py](./1-top_ten.py)
